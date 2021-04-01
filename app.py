7fa80b01a78a643801b3ee68dd37711c57d5063f
name <infra>
2021-03-31 23:43:44 -0400

    black up

--- app.py
+++ app.py
@@ -14,21 +14,22 @@ from youtube_dl.version import __version__ as youtube_dl_version
 
 from flask import Flask, render_template
 
-service = os.environ.get('K_SERVICE', 'Unknown service')
-revision = os.environ.get('K_REVISION', 'Unknown revision')
+service = os.environ.get("K_SERVICE", "Unknown service")
+revision = os.environ.get("K_REVISION", "Unknown revision")
 
 
-if not hasattr(sys.stderr, 'isatty'):
+if not hasattr(sys.stderr, "isatty"):
     # In GAE it's not defined and we must monkeypatch
     sys.stderr.isatty = lambda: False
 
 from json import dumps, loads
 from flask import make_response
 
+
 def jsonify(arg, status=200, indent=4, sort_keys=True, **kwargs):
     response = make_response(dumps(dict(arg), indent=indent, sort_keys=sort_keys))
-    response.headers['Content-Type'] = 'text/plain; charset=utf-8'
-    response.headers['mimetype'] = 'text/plain'
+    response.headers["Content-Type"] = "text/plain; charset=utf-8"
+    response.headers["mimetype"] = "text/plain"
     response.status_code = status
     return response
 
@@ -40,13 +41,13 @@ class SimpleYDL(youtube_dl.YoutubeDL):
 
 
 def get_videos(url, extra_params):
-    '''
+    """
     Get a list with a dict for every video founded
-    '''
+    """
     ydl_params = {
-        'format': 'best',
-        'cachedir': False,
-        'logger': current_app.logger.getChild('youtube-dl'),
+        "format": "best",
+        "cachedir": False,
+        "logger": current_app.logger.getChild("youtube-dl"),
     }
     ydl_params.update(extra_params)
     ydl = SimpleYDL(ydl_params)
@@ -55,33 +56,34 @@ def get_videos(url, extra_params):
 
 
 def flatten_result(result):
-    r_type = result.get('_type', 'video')
-    if r_type == 'video':
+    r_type = result.get("_type", "video")
+    if r_type == "video":
         videos = [result]
-    elif r_type == 'playlist':
+    elif r_type == "playlist":
         videos = []
-        for entry in result['entries']:
+        for entry in result["entries"]:
             videos.extend(flatten_result(entry))
-    elif r_type == 'compat_list':
+    elif r_type == "compat_list":
         videos = []
-        for r in result['entries']:
+        for r in result["entries"]:
             videos.extend(flatten_result(r))
     return videos
 
 
-api = Blueprint('api', __name__)
+api = Blueprint("api", __name__)
 
 
 def route_api(subpath, *args, **kargs):
-    return api.route('/api/' + subpath, *args, **kargs)
+    return api.route("/api/" + subpath, *args, **kargs)
 
 
 def set_access_control(f):
     @functools.wraps(f)
     def wrapper(*args, **kargs):
         response = f(*args, **kargs)
-        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers["Access-Control-Allow-Origin"] = "*"
         return response
+
     return wrapper
 
 
@@ -89,7 +91,7 @@ def set_access_control(f):
 @api.errorhandler(youtube_dl.utils.ExtractorError)
 def handle_youtube_dl_error(error):
     logging.error(traceback.format_exc())
-    result = jsonify({'error': str(error)})
+    result = jsonify({"error": str(error)})
     result.status_code = 500
     return result
 
@@ -103,7 +105,7 @@ class WrongParameterTypeError(ValueError):
 @api.errorhandler(WrongParameterTypeError)
 def handle_wrong_parameter(error):
     logging.error(traceback.format_exc())
-    result = jsonify({'error': str(error)})
+    result = jsonify({"error": str(error)})
     result.status_code = 400
     return result
 
@@ -111,7 +113,7 @@ def handle_wrong_parameter(error):
 @api.before_request
 def block_on_user_agent():
     user_agent = request.user_agent.string
-    forbidden_uas = current_app.config.get('FORBIDDEN_USER_AGENTS', [])
+    forbidden_uas = current_app.config.get("FORBIDDEN_USER_AGENTS", [])
     if user_agent in forbidden_uas:
         abort(429)
 
@@ -120,32 +122,32 @@ def query_bool(value, name, default=None):
     if value is None:
         return default
     value = value.lower()
-    if value == 'true':
+    if value == "true":
         return True
-    elif value == 'false':
+    elif value == "false":
         return False
     else:
-        raise WrongParameterTypeError(value, 'bool', name)
+        raise WrongParameterTypeError(value, "bool", name)
 
 
 ALLOWED_EXTRA_PARAMS = {
-    'format': str,
-    'playliststart': int,
-    'playlistend': int,
-    'playlist_items': str,
-    'playlistreverse': bool,
-    'matchtitle': str,
-    'rejecttitle': str,
-    'writesubtitles': bool,
-    'writeautomaticsub': bool,
-    'allsubtitles': bool,
-    'subtitlesformat': str,
-    'subtitleslangs': list,
+    "format": str,
+    "playliststart": int,
+    "playlistend": int,
+    "playlist_items": str,
+    "playlistreverse": bool,
+    "matchtitle": str,
+    "rejecttitle": str,
+    "writesubtitles": bool,
+    "writeautomaticsub": bool,
+    "allsubtitles": bool,
+    "subtitlesformat": str,
+    "subtitleslangs": list,
 }
 
 
 def get_result():
-    url = request.args['url']
+    url = request.args["url"]
     extra_params = {}
     for k, v in request.args.items():
         if k in ALLOWED_EXTRA_PARAMS:
@@ -153,71 +155,77 @@ def get_result():
             if convertf == bool:
                 convertf = lambda x: query_bool(x, k)
             elif convertf == list:
-                convertf = lambda x: x.split(',')
+                convertf = lambda x: x.split(",")
             extra_params[k] = convertf(v)
     return get_videos(url, extra_params)
 
 
-@route_api('info')
+@route_api("info")
 @set_access_control
 def info():
-    url = request.args['url']
+    url = request.args["url"]
     result = get_result()
-    key = 'info'
-    if query_bool(request.args.get('flatten'), 'flatten', False):
+    key = "info"
+    if query_bool(request.args.get("flatten"), "flatten", False):
         result = flatten_result(result)
-        key = 'videos'
+        key = "videos"
     return jsonify(result)
 
 
-@route_api('play')
+@route_api("play")
 def play():
     result = flatten_result(get_result())
-    return redirect(result[0]['url'])
+    return redirect(result[0]["url"])
 
 
-@route_api('extractors')
+@route_api("extractors")
 @set_access_control
 def list_extractors():
-    ie_list = [{
-        'name': ie.IE_NAME,
-        'working': ie.working(),
-    } for ie in youtube_dl.gen_extractors()]
-    return jsonify({'extractors':ie_list})
+    ie_list = [
+        {
+            "name": ie.IE_NAME,
+            "working": ie.working(),
+        }
+        for ie in youtube_dl.gen_extractors()
+    ]
+    return jsonify({"extractors": ie_list})
 
 
-@route_api('version')
+@route_api("version")
 @set_access_control
 def version():
     result = {
-        'youtube-dl': youtube_dl_version,
-        'youtube-dl-api-server': 0.1,
-        'twint': utwee.twint_version,
+        "youtube-dl": youtube_dl_version,
+        "youtube-dl-api-server": 0.1,
+        "twint": utwee.twint_version,
     }
     return jsonify(result)
 
-@route_api('current_ip')
+
+@route_api("current_ip")
 @set_access_control
 def current_ip():
-    res = urllib.request.urlopen('https://ipinfo.io').read().decode()
+    res = urllib.request.urlopen("https://ipinfo.io").read().decode()
     return jsonify(loads(res))
 
-@route_api('headers')
+
+@route_api("headers")
 @set_access_control
 def headers():
     return jsonify(dict(request.headers))
 
-@route_api('tweep')
+
+@route_api("tweep")
 @set_access_control
 def tweep():
     username = request.args["username"]
     limit = int(request.args["limit"] or 100)
-    return Response(utwee.generate_response(username, limit), mimetype='text/plain')
+    return Response(utwee.generate_response(username, limit), mimetype="text/plain")
+
 
 app = Flask("__main__")
 app.register_blueprint(api)
 
-if __name__ == '__main__':
-    server_port = os.environ.get('PORT', '8080')
-    app.run(debug=False, port=server_port, host='0.0.0.0')
-
+if __name__ == "__main__":
+    server_port = os.environ.get("PORT", "8080")
+    app.run(debug=False, port=server_port, host="0.0.0.0")
